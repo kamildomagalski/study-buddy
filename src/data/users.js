69e5a8872db63a1885c33130cb971e05d7aad04c
Kamil Domagalski <domagalski.kamil@gmail.com>
2021-10-03 19:50:13 +0200

    [Add] user list and user list item components

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    name: 'Adam Adamski',
+    attendance: '56%',
+    average: '4.1',
+  },
+  {
+    name: 'Bartek Bartecki',
+    attendance: '66%',
+    average: '3.9',
+  },
+  {
+    name: 'Celina Cichy',
+    attendance: '46%',
+    average: '4.5',
+  },
+  {
+    name: 'Dawid Domański',
+    attendance: '76%',
+    average: '4.9',
+  },
+  {
+    name: 'Edward Emski',
+    attendance: '46%',
+    average: '2.9',
+  },
+  {
+    name: 'Felix Fajka',
+    average: '3.5',
+  },
+];
