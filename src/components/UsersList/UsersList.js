7cef83b4ce393ea2a4ab6af48743de302e44cdf0
Kamil Domagalski <domagalski.kamil@gmail.com>
2021-10-09 15:51:02 +0200

    [Add] styled components with basic styling

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { users } from 'data/users';
 import UsersListItem from 'components/UsersListItem/UsersListItem';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  background-color: white;
+  width: 100%;
+  max-width: 500px;
+  padding: 40px 30px;
+  border-radius: 25px;
+  box-shadow: -2px 4px 10px 0px #737c8e17;
+`;
+
+const StyledList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
 function UsersList() {
   return (
-    <div>
+    <Wrapper>
       <ul>
         {users.map((userData) => (
           <UsersListItem userData={userData} />
         ))}
       </ul>
-    </div>
+    </Wrapper>
   );
 }
 
